84d685a6581ee9af61fbed5d54763daa61011af0
Pedro Cardoso da Silva (@forsureitsme) <forsureitsme@gmail.com>
2024-05-04 17:33:48 -0300

    Estado de jogo de duas cartas diferentes selecionadas (closes #4)

--- scripts/game.js
+++ scripts/game.js
@@ -6,12 +6,14 @@ class Card {
   cardFront = document.createElement("div");
   foreground = 0;
 
-  constructor(foregroundIndex) {
+  constructor(foreground) {
+    this.foreground = foreground;
+
     this.element.classList.add("memory__card");
     this.cardFront.classList.add("memory__card_front");
     this.cardBack.classList.add("memory__card_back");
     this.cardFront.classList.add(
-      `memory__card_front--foreground-${foregroundIndex}`
+      `memory__card_front--foreground-${foreground}`
     );
 
     this.element.appendChild(this.cardFront);
@@ -23,6 +25,8 @@ class Card {
       "memory__card--revealed",
       this.matched || this.selected
     );
+
+    this.element.classList.toggle("memory__card--matched", this.matched);
   }
 }
 
@@ -57,20 +61,70 @@ class Game {
     // Adiciona as cartas na mesa
     for (let i = 0; i < this.board.length; i++) {
       const card = this.board[i];
-      
+
       // Atribui evento de clique no elemento da carta como seleção
-      card.element.addEventListener('click', () => this.selectCard(card));
+      card.element.addEventListener("click", () => this.selectCard(card));
 
       this.table.appendChild(card.element);
     }
   }
 
   selectCard(card) {
+    // Ignorar cartas já combinadas
+    if (card.matched) {
+      return;
+    }
+
+    // Marca carta como selecionada
     card.selected = true;
     card.render();
+
+    this.checkMatch();
+  }
+
+  checkMatch() {
+    
+    const selectedCards = this.board.filter((card) => card.selected);
+
+    // Prossegue com a verificação de combinação somente se houver escolhido duas cartas
+    if (selectedCards.length < 2) {
+      return;
+    }
+
+    // Verifica se cards selecionados possuem a mesma face
+    if (
+      selectedCards.every(
+        (card) => card.foreground === selectedCards[0].foreground
+      )
+    ) {
+      // TODO: Marcar cartas como combinadas
+      return;
+    }
+
+    // Não sendo os cards de mesma face, bloquear ações no tabuleiro por 1.5 segundos
+    // e então desselecionar cartas e permitir ações novamente
+    this.toggleLock(true);
+    setTimeout(() => {
+      this.unselectCards(selectedCards);
+      this.toggleLock(false);
+    }, 1500);
+  }
+
+  // Marca cartas como não selecionadas, escondendo elas
+  unselectCards(cards) {
+    for (let i = cards.length - 1; i >= 0; i--) {
+      cards[i].selected = false;
+      cards[i].render();
+    }
+  }
+
+  // Bloqueia ações no tabuleiro, previnindo eventos do mouse
+  toggleLock(state) {
+    this.table.classList.toggle("memory__table--locked", state);
   }
 }
 
+// Inicia jogo
 const table = document.querySelector(".memory__table");
 const game = new Game(table);
 game.init();
